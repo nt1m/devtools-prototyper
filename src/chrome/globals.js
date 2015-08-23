08f41d050c89141630eab8c4c55c31c9c9c11187
Tim Nguyen <ntim.bugs@gmail.com>
2015-08-23 16:27:21 +0200

    remove unused events

--- src/chrome/globals.js
+++ src/chrome/globals.js
@@ -7,9 +7,3 @@ const {require} = Cu.import("resource://gre/modules/devtools/Loader.jsm", {});
 const {ViewHelpers} = require("resource:///modules/devtools/ViewHelpers.jsm");
 const L10N = new ViewHelpers.L10N(`${basePath}/locale/strings.properties`);
 const tabs = require("sdk/tabs");
-
-const Events = {
-	LIBRARY_CHANGE: "Prototyper::LibraryChange",
-	CODE_RUN: "Prototyper::CodeRun",
-	SETTINGS_CHANGE: "Prototyper::SettingsChange"
-};
