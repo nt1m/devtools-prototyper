2a6abee4d05ea698b5bec9bbb62de78dbdb86682
Mohamed <mohamed@boulo.ch>
2020-10-20 22:33:16 +0200

    add shorcuts to launch

--- ui/components/toolbar/toolbar.js
+++ ui/components/toolbar/toolbar.js
@@ -10,7 +10,16 @@ export default class Toolbar extends BaseElement {
   stylesheets = ["components/toolbar/toolbar.css"]
   buttons = []
   connected() {
-    this.addButton({name: "Launch", action: () => document.dispatchEvent(new Event('launch'))});
+    const launch = this.addButton({name: "Launch", action: () => document.dispatchEvent(new Event('launch'))});
+    document.addEventListener('keydown', e => {
+      const ctrl_s = e.ctrlKey && e.code === "KeyS";
+      const ctrl_enter = e.ctrlKey && e.code === "Enter";
+      if (ctrl_s || ctrl_enter) {
+        e.stopPropagation();
+        e.preventDefault();
+        launch.click();
+      }
+    })
   }
   addButton({icon, name, alt, position, action} = {}) {
     name = name || "";
@@ -22,6 +31,7 @@ export default class Toolbar extends BaseElement {
     el.innerText = name;
     el.alt = alt;
     this.shadowRoot.append(el);
+    return el;
   }
   addSeparator() {
     this.shadowRoot.append(document.createElement('hr'));
