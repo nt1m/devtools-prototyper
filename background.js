2432b7a4969d314c65535e62ec2132e77a0b1b99
Mohamed <mohamed@boulo.ch>
2020-10-20 18:17:17 +0200

    use the same tab to show results

--- background.js
+++ background.js
@@ -1,16 +1,23 @@
 "use strict";
 let tab = null;
+chrome.tabs.onRemoved.addListener(id => {
+    if (tab && tab.id === id) tab = null;
+})
 chrome.runtime.onConnect.addListener(function(devToolsConnection) {
   let devToolsListener = async function({doc, js}, sender, sendResponse) {
-    console.log("reponse", await chrome.tabs.create({url: "vide.html"}, t => {
+    const show = () => chrome.tabs.executeScript(tab.id,
+        {
+            code:`document.documentElement.innerHTML = 
+                \`${doc.split('`').join('\\`')}\`;
+            ${js}`
+        });
+    if (tab) {
+        chrome.tabs.update(tab.id, {active: true})
+        show ()
+    } else chrome.tabs.create({url: "vide.html"}, t => {
         tab = t;
-        chrome.tabs.executeScript(t.id,
-            {
-                code:`document.documentElement.innerHTML = 
-                    \`${doc.split('`').join('\\`')}\`;
-                ${js}`
-            });
-    }));
+        show ()
+    });
   };
   devToolsConnection.onMessage.addListener(devToolsListener);
 
